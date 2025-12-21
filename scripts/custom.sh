57cfc27338a4026562f0ce0ef8dbf2db94b277a8
RoudineBWT <roudinebwt@proton.me>
2025-12-21 01:44:30 +0100

    change a lot of things

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -2,17 +2,17 @@
 
 # custom name
 HOME_URL="https://github.com/RoudineBWT/okadora"
-echo "okadora" | tee "/etc/hostname"
+echo "Okadora" | tee "/etc/hostname"
 
 sed -i -f - /usr/lib/os-release <<EOF
-s|^NAME=.*|NAME=\"okadora\"|
-s|^PRETTY_NAME=.*|PRETTY_NAME=\"okadora\"|
+s|^NAME=.*|NAME=\"Okadora\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"Okadora\"|
 s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"Posture\"|
 s|^VARIANT_ID=.*|VARIANT_ID=""|
 s|^HOME_URL=.*|HOME_URL=\"${HOME_URL}\"|
-s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:matthias-adr:okadora\"|
+s|^CPE_NAME=\".*\"|CPE_NAME=\"cpe:/o:RoudineBWT:okadora\"|
 s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"${HOME_URL}\"|
-s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="okadora"|
+s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME="Okadora"|
 
 /^REDHAT_BUGZILLA_PRODUCT=/d
 /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d
---
+++ system_files/usr/etc/profile.d/bazzite-neofetch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Okadora Fastfetch configuration (replacing Bazzite defaults)
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/okadora/fastfetch.jsonc'
+alias neofetch='/usr/bin/fastfetch -c /usr/share/okadora/fastfetch.jsonc'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/okadora/fastfetch.jsonc'
+alias hyfetch='/usr/bin/fastfetch -c /usr/share/okadora/fastfetch.jsonc'
